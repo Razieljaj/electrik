436d4395bbb16ae55b1a2fa5e3cd169833dba75b
Ewen <ewen.rigaud@gmail.com>
2024-09-18 16:46:49 +0200

    Premier commit ( c'est faux )

---
+++ main.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Electrik</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<h1>Escape game</h1>
+  <p>Choisissez la difficulté:</p>
+  <div class="difficulte">
+    <a type="button" class="button button1">Gueux</a>
+    <a type="button" class="button button1">Moyen</a>
+    <a type="button" class="button button1">Polytechnique</a>
+  </div>
+
+</body>
+</html>
