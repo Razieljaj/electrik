2ff75406ddaa3ba8a6207824980bb2f8e85d2d36
Ewen <ewen.rigaud@gmail.com>
2024-09-18 16:30:56 +0200

    encore un test pour voir si ca marche

---
+++ main.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Electrik</title>
+</head>
+<body>
+    <h1>
+        j'aime la compotée de yaourt
+    </h1>
+    <p>les saucisses sont cuites</p>
+</body>
+</html>
