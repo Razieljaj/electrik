8198b3f9363f02b3b97e7668726a315d439b5a50
Ewen <ewen.rigaud@gmail.com>
2024-09-21 13:06:18 +0200

    modification du css, ajout de l anguille et d animations

--- main.php
+++ main.php
@@ -1,19 +1,20 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Electrik</title>
     <link rel="stylesheet" href="style.css">
+    <title>Electrik</title>
 </head>
 <body>
 
-<h1>Escape game</h1>
-  <p>Choisissez la difficulté:</p>
+<h1 class = "titre">Escape game</h1>
+  <p class = 'choix'>Choisissez la difficulté:</p>
   <div class="difficulte">
     <a type="button" class="button button1">Gueux</a>
-    <a type="button" class="button button1">Moyen</a>
-    <a type="button" class="button button1">Polytechnique</a>
+    <a type="button" class="button button2">Moyen</a>
+    <a type="button" class="button button3">Polytechnique</a>
   </div>
+  <img src="image\eel\eel-content.png" class="eel-content">
 </body>
 </html>
