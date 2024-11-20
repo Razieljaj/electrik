c3535f8a5d3c1d87528da09ebd04bc812c0177bd
Ewen <ewen.rigaud@gmail.com>
2024-11-20 14:53:45 +0100

    V1.0.1, l'ancre a été ajoutée a la page.

--- certif/script.js
+++ certif/script.js
@@ -1,18 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var timer = 60;
+    var timer = 1; // Durée du compte à rebours en secondes
     var countdown = setInterval(function () {
         timer--;
-        document.getElementById('timer').textContent = timer;
+        document.getElementById('timer').textContent = timer; // Mettre à jour l'affichage du timer
 
         if (timer <= 0) {
-            clearInterval(countdown);  // Arrêter le timer une fois qu'il atteint zéro
-            document.getElementById('button').style.display = 'block';  // Afficher le bouton "Vérifier"
+            clearInterval(countdown); // Arrêter le timer une fois qu'il atteint zéro
+            document.getElementById('button').style.display = 'block'; // Afficher le bouton "Vérifier"
         }
-    }, 1100);
+    }, 1000); // Compte à rebours en millisecondes (1 seconde = 1000 ms)
 
-    // Lorsque le bouton "Vérifier" est cliqué
+    // Action à exécuter lorsque le bouton "Vérifier" est cliqué
     document.getElementById('button').onclick = function () {
-        document.getElementById('felicitation').style.display = "block";  // Afficher le message de félicitations
-        document.getElementById('felicitationButton').style.display = "block";  // Afficher le bouton "Continuer"
+        const felicitationSection = document.getElementById('felicitation');
+        const felicitationButton = document.getElementById('felicitationButton');
+
+        // Afficher les félicitations et le bouton "Continuer"
+        felicitationSection.style.display = "block";
+        felicitationButton.style.display = "block";
+
+        // Faire défiler la page jusqu'à la section Félicitation
+        felicitationSection.scrollIntoView({ behavior: "smooth", block: "start" });
     };
 });
